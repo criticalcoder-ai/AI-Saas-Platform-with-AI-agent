74c70d5e08cfab5a8c35d5d0d511e0f1d4882fb6
Hmazah <alhamza2012@gmail.com>
2025-03-12 10:00:00 +0200

    add Cleark

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Variable } from "lucide-react";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,10 +21,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
+      <ClerkProvider appearance={{variables:{colorPrimary:'#fe5933'}}}>
+
+        
         <Navbar/>
-        {children}</body>
+        {children}
+
+        </ClerkProvider>
+
+        </body>
+        
     </html>
+    
   );
 }
