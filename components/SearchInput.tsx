0e6d0fe821d337a439e15f64a4344cc4a3b2cc17
Hmazah <alhamza2012@gmail.com>
2025-03-20 10:00:00 +0200

    add Cleark

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -3,6 +3,7 @@
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
 import {useEffect, useState} from "react";
 import Image from "next/image";
+import {formUrlQuery, removeKeysFromUrlQuery} from "@jsmastery/utils";
 
 const SearchInput = () => {
     const pathname = usePathname();
@@ -12,28 +13,28 @@ const SearchInput = () => {
 
     const [searchQuery, setSearchQuery] = useState('');
 
-    // useEffect(() => {
-    //     const delayDebounceFn = setTimeout(() => {
-    //         if(searchQuery) {
-    //             const newUrl = formUrlQuery({
-    //                 params: searchParams.toString(),
-    //                 key: "topic",
-    //                 value: searchQuery,
-    //             });
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if(searchQuery) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: "topic",
+                    value: searchQuery,
+                });
 
-    //             router.push(newUrl, { scroll: false });
-    //         } else {
-    //             if(pathname === '/companions') {
-    //                 const newUrl = removeKeysFromUrlQuery({
-    //                     params: searchParams.toString(),
-    //                     keysToRemove: ["topic"],
-    //                 });
+                router.push(newUrl, { scroll: false });
+            } else {
+                if(pathname === '/companions') {
+                    const newUrl = removeKeysFromUrlQuery({
+                        params: searchParams.toString(),
+                        keysToRemove: ["topic"],
+                    });
 
-    //                 router.push(newUrl, { scroll: false });
-    //             }
-    //         }
-    //     }, 500)
-    // }, [searchQuery, router, searchParams, pathname]);
+                    router.push(newUrl, { scroll: false });
+                }
+            }
+        }, 500)
+    }, [searchQuery, router, searchParams, pathname]);
 
     return (
         <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
