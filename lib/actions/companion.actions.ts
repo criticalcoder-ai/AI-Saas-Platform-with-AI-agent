b62301e2012dd7698ee2c1ecdb810ba8135c894e
Hmazah <alhamza2012@gmail.com>
2025-03-25 10:00:00 +0200

    add vapi

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -68,3 +68,15 @@ export const newCompanionPermissions = async () => {
 }
 
 }
+export const getCompanion = async (id: string) => {
+    const supabase = createSupabaseClient();
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('id', id);
+
+    if(error) return console.log(error);
+
+    return data[0];
+}
