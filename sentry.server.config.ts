5091382ecd1d687c141e5133d2eafd66300efc5b
Hmazah <alhamza2012@gmail.com>
2025-03-26 10:00:00 +0200

    add companion cmponnent

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://db5ea33332f34d548170ddc02a4c6bee@o4509508073947136.ingest.de.sentry.io/4509530393083984",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
